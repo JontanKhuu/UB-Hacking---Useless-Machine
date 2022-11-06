222d32bc5be4b10d542c5aea768bcb2ee45cd37d
JontanKhuu <jontank424@gmail.com>
2022-11-05 21:09:39 -0400

    Co-authored-by: UBEldonWu <UBEldonWu@users.noreply.github.com> Co-authored-by: JunHaoXia <JunHaoXia@users.noreply.github.com> Co-authored-by: kjsong10 <kjsong10@users.noreply.github.com>

--- lever.js
+++ lever.js
@@ -1,25 +1,43 @@
-//import { mouseControl } from "./MouseControl";
 const canvas = document.getElementById("screen");
 const context = canvas.getContext('2d')
 
+const img1 = new Image();
+img1.src = "frames/LeverDown.png";
+
+
+const img2 = new Image();
+img2.src = "frames/LeverUp.png";
+
+const frameSheet = new Image();
+frameSheet = "frames/Frame Sheet.png";
+const boxSize = 800
+
 export default class Lever{
-    constructor(x,y){
-        this.x = x; //top left
-        this.y = y; //top left
-    
+    constructor(balls){
+        this.leverActive = false;
+        let gameFrame = 0;
+        const stagFrame = 70;
     }
-
+    leverClicked(){
+        console.log("Inside leverClicked")
+        this.leverActive = !this.leverActive;
+    }
+  
     draw() {
-        context.fillStyle = "blue";
-        context.fillRect(100,150,this.x,this.y);
+        console.log("Lever is current Active: " + this.leverActive)
+        if (this.leverActive == true) {
+            context.drawImage(img2,0,0)
+        }
+        else {
+            context.drawImage(img1, 0,0);
+        }
     }
-
-
 
 }
 
 
-
+/* lever down -> lever up -> 5 sec delay -> open frame 1-3 -> fully open ->
+   movement 1-3 -> close 1-5 -> lever down  */
 
 
 
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-
-export default class Test{
+import Lever from "./lever.js"
+export default class Mouse{
     constructor(canvas){
         this.canvas = canvas
         function getMousePos(canvas, evt) {
@@ -12,10 +12,18 @@ export default class Test{
         canvas.addEventListener('click', function(evt) {
             event.preventDefault();
             var mousePos = getMousePos(canvas, evt);
-            console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+            printMousePosition(mousePos.x, mousePos.y)
         }, false);
         canvas.addEventListener('contextmenu',event => {
             event.preventDefault();
         })
+        function printMousePosition(x,y){
+            console.log(x, y)
+        }
+        function checkOnLever(x,y){
+            //get lever positions
+            //check if clicked on positions
+            //call clickedLever function from leverClass
+        }
     }  
 }
